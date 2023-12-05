9a7f50cd91d8268fed96bb2d5efbbf0cca6653f9
Ihor Melnyk <igor.melnick85@gmail.com>
2023-12-05 10:41:30 +0200

    added function setPropRegistration

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ function setHiddenReg() {
   controlHidden("RegDate");
   controlHidden("RegNumber");
 }
+
+function SendOutDocTask() {
+  debugger;
+  var stateTask = EdocsApi.getCaseTaskDataByCode("SendOutDoc")?.state;
+
+  if (stateTask == "assigned" || stateTask == "inProgress" || stateTask == "delegated" || stateTask == "completed") {
+    controlHidden("RegDate", false);
+    controlHidden("RegNumber", false);
+  } else {
+    controlHidden("RegDate");
+    controlHidden("RegNumber");
+  }
+}
+
